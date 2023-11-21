f9c8b984f343df53c3f6b43cd39255e206d6216e
Italo Portinho <italons@gmail.com>
2023-11-21 08:41:45 -0300

    Correções no insert. Delete finalizado (ainda sem fixup).

--- main.cpp
+++ main.cpp
@@ -5,15 +5,22 @@
 
 int main() {
     ArvoreRedBlack* minha_arvore = new ArvoreRedBlack { .raiz = 0};
-    Insert(minha_arvore, 11);
-    Insert(minha_arvore, 2);
-    Insert(minha_arvore, 14);
-    Insert(minha_arvore, 15);
-    Insert(minha_arvore, 8);
-    Insert(minha_arvore, 7);
-    Insert(minha_arvore, 4);
-    Insert(minha_arvore, 1);
-    Insert(minha_arvore, 5);
+    NoArvore* no_onze = Insert(minha_arvore, 11);
+    NoArvore* no_dois = Insert(minha_arvore, 2);
+    NoArvore* no_catorze = Insert(minha_arvore, 14);
+    NoArvore* no_quinze = Insert(minha_arvore, 15);
+    NoArvore* no_oito = Insert(minha_arvore, 8);
+    NoArvore* no_sete = Insert(minha_arvore, 7);
+    NoArvore* no_quatro  = Insert(minha_arvore, 4);
+    NoArvore* no_um  = Insert(minha_arvore, 1);
+    NoArvore* no_cinco  = Insert(minha_arvore, 5);
     em_ordem(minha_arvore->raiz);
-    printf("RAIZ == %d", minha_arvore->raiz->dado);
+    printf("\nRAIZ == %d\n", minha_arvore->raiz->dado);
+
+    Delete(minha_arvore, 14);
+    em_ordem(minha_arvore->raiz);
+    printf("\nRAIZ == %d\n", minha_arvore->raiz->dado);
+    // no_oito = Search(minha_arvore, 8);
+    // printf("\nPai do 8 ==  %d\n", no_oito->pai);
+    // printf("\nPai do 11 ==  %d\n", no_onze->pai->dado);
 }
--- redblack/redblack.hpp
+++ redblack/redblack.hpp
@@ -75,6 +75,9 @@ void RightRotate(ArvoreRedBlack* arvore, NoArvore* no) {
     // O pai de y agora é o pai de x.
     // Este trecho é igual a LeftRotate
     aux_y->pai = no->pai;
+    if (aux_y->dir) {
+        aux_y->dir->pai = no;
+    }
 
     // Definir se y fica a esq ou dir do pai do no x (se houver).
     // Este trecho é igual a LeftRotate
@@ -124,7 +127,7 @@ void LeftRotate(ArvoreRedBlack* arvore, NoArvore* no) {
     return;
 }
 
-void _rbinsert_fixup(ArvoreRedBlack* arvore, NoArvore* no) {
+NoArvore* _rbinsert_fixup(ArvoreRedBlack* arvore, NoArvore* no) {
     // O no inserido (z) é sempre vermelho.
     // Se z nao tiver pai, entao é a raíz e sera colorido no final da função.
     // Se z tiver pai e este for vermelho, é uma violação da regra RB e será
@@ -192,10 +195,10 @@ void _rbinsert_fixup(ArvoreRedBlack* arvore, NoArvore* no) {
         }
     }
     arvore->raiz->cor = BLACK;
-    return;
+    return no;
 }
 
-void _rbinsert(ArvoreRedBlack* arvore, NoArvore* no) {
+NoArvore* _rbinsert(ArvoreRedBlack* arvore, NoArvore* no) {
     // Fazer o percurso descendente na árvore,
     // guardando o pai(y), para encontrar a posição de inserção(x).
     // Complexidade: O(lg n)
@@ -226,11 +229,13 @@ void _rbinsert(ArvoreRedBlack* arvore, NoArvore* no) {
     // Ponteiros esquerdo e direito do no sao null, e sua cor é RED
     // Chamar RB-INSERT-FIXUP: recolorir e rotacionar a árvore,
     // para mante-la balanceada.
-    _rbinsert_fixup(arvore, no);
-    return;
+    return _rbinsert_fixup(arvore, no);
 }
 
-void Insert(ArvoreRedBlack* arvore, int elemento) {
+NoArvore* Insert(ArvoreRedBlack* arvore, int elemento) {
+    printf("\n*************************************************\n");
+    printf("**************** Inserindo %d *******************", elemento);
+    printf("\n*************************************************\n");
     NoArvore* new_node = new NoArvore {
         .dado = elemento
         , .cor = RED
@@ -238,14 +243,139 @@ void Insert(ArvoreRedBlack* arvore, int elemento) {
         , .dir = 0
         , .pai = 0
     };
-    _rbinsert(arvore, new_node);
+
+    return _rbinsert(arvore, new_node);
+}
+
+NoArvore* TreeMinimum(NoArvore* no) {
+    // COMPLEXIDADE: O(lg_n) - percorre um caminho descendente na árvore0
+    NoArvore* aux_x = no;
+    while (aux_x->esq != 0) {
+        // printf("\nTreeMinimum - no == %d\n", aux_x->dado);
+        aux_x = aux_x->esq;
+    }
+    // printf("\nTreeMinimum final == %d\n", aux_x->dado);
+    return aux_x;
+}
+
+NoArvore* TreeSucessor(NoArvore* no) {
+    // COMPLEXIDADE: O(lg_n) - percorre no maximo um caminho descendente ou
+    // ascendente na árvore.
+    // Se houver filho direito, o sucesso sera o minimo dessa subarvore.
+    NoArvore* aux_x = no;
+    if (aux_x->dir != 0) {
+        return TreeMinimum(aux_x->dir);
+    }
+    NoArvore* aux_y = aux_x->pai;
+    // Nao havendo filho direito, subimos na arvore atualizando y e o no (x)
+    // para seus pais, até que y seja um filho da esquerda, ou nulo(raíz)
+    while (aux_y != 0 && aux_x == aux_y->dir) {
+        aux_x = aux_y;
+        aux_y = aux_y->pai;
+    }
+    return aux_y;
+}
+
+NoArvore* Search(ArvoreRedBlack* arvore, int elemento) {
+    NoArvore* no = arvore->raiz;
+    while (no != 0 && no->dado != elemento) {
+        if (elemento < no->dado) {
+            no = no->esq;
+        } else {
+            no = no->dir;
+        }
+    }
+    printf("\nSeacrh == %d\n", no->dado);
+    return no;
+}
+
+void _rbdelete_fixup(ArvoreRedBlack* arvore, NoArvore* no) {
     return;
 }
 
-void Delete(NoArvore* no) {
+void _rbdelete(ArvoreRedBlack* arvore, NoArvore* no) {
+    NoArvore* aux_y = 0;
+    NoArvore* aux_x = 0;
+    if (no->esq == 0) {
+        if (no->dir == 0) {
+            // CASO 1: nó z, nao tem filhos.
+            printf("\nCASO 1: no z, nao tem filhos.\n");
+            // Atualizamos o ponteiro do pai para null.
+            if (no == no->pai->esq) {
+                no->pai->esq = 0;
+            } else {
+                no->pai->dir = 0;
+            }
+            no = 0;
+        } else {
+            printf("\nCASO 2: no z tem apenas um filho(dir).\n");
+            // CASO 2: nó z tem apenas um filho.
+            // nó z tem filho direito.
+            // definir se nó z é filho direito ou esquerdo de seu pai.
+            if (no == no->pai->esq) {
+                no->pai->esq = no->dir;
+            } else {
+                no->pai->dir = no->dir;
+            }
+            no->dir->pai = no->pai;
+            no = 0;
+        }
+    } else {
+        // nó z tem pelo menos filho esquerdo.
+        if (no->dir == 0) {
+            printf("\nCASO 2: no z tem apenas um filho(esq).\n");
+            // CASO 2: nó z tem apenas um filho.
+            // nó z tem filho esquerdo.
+            // definir se nó z é filho direito ou esquerdo de seu pai.
+            // E atualizar o filho esq/dir do pai do no com
+            // o filho esquerdo do nó
+            if (no == no->pai->esq) {
+                no->pai->esq = no->esq;
+            } else {
+                no->pai->dir = no->esq;
+            }
+            no = 0;
+        } else {
+            // CASO 3: nó z tem os dois filhos.
+            // Substituir nó z pelo seu tree-sucessor.
+            // O sucessor de um  nó não tem filho da esquerda.
+            // OBS: o sucessor ou é filho imediato da direita,
+            // ou é filho da esquerda de alguem na sub-arvore da direita.
+            printf("\nCASO 3: no z tem os dois filhos.\n");
+            aux_y = TreeSucessor(no);
+            printf("\n Y = %d.\n", aux_y->pai->dado);
+            aux_x = aux_y->dir;  // pois o sucessor nao tem esquerda
+            no->dado = aux_y->dado;
+            printf("\nfilho da direita da raiz == %d\n", no->dir);
+            // verificar se y é filho da esquerda ou direita
+            // e atribuir x. x é o temporario com a sub-arvore filha de y.
+            if (aux_y == aux_y->pai->esq) {
+                printf("\nIFFFF\n");
+                aux_y->pai->esq = aux_x;
+                printf("\nfilho da direita da raiz == %d\n", no->dir);
+            } else {
+                printf("\nELSEEE\n");
+                aux_y->pai->dir = aux_x;
+                printf("\nfilho da direita da raiz == %d\n", no->dir);
+            }
+            // printf("\nfilho da direita da raiz == %d\n", no->dir);
+            // aux_y = 0;
+            // printf("\nfilho da direita da raiz == %d\n", no->dir->dado);
+        }
+    }
+    if (aux_y && aux_y->cor == BLACK) {
+        _rbdelete_fixup(arvore, aux_x);
+    }
     return;
 }
 
-void Search(int elemento) {
+void Delete(ArvoreRedBlack* arvore, int elemento) {
+    NoArvore* no = Search(arvore, elemento);
+    if (no) {
+        printf("\nElemento [ %d ] encontrado. Prosseguindo com delete.\n", elemento);
+        return _rbdelete(arvore, no);
+    }
+    printf("\nElemento [ %d ] nao pertence a arvore.\n", elemento);
     return;
 }
+
